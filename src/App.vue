37153b3b26da0287153ffbfac80b892f6c29fed6
rana haleem <ranahaleem09@gmail.com>
2021-07-17 14:05:11 +0200

    Setting the project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+  <VideoBanner/>
+  <Notifications/>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import VideoBanner from '@/components/VideoBanner.vue'
+import Notifications from '@/components/Notifications.vue'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    VideoBanner,
+    Notifications
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
