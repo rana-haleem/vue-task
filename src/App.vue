014522bbe87c4ef2f1e8b3806b861ba5141d1e63
rana haleem <ranahaleem09@gmail.com>
2021-07-17 16:21:39 +0200

    adding images

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <Header />
   <VideoBanner/>
   <Notifications/>
+  <Next/>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import VideoBanner from '@/components/VideoBanner.vue'
 import Notifications from '@/components/Notifications.vue'
+import Next from '@/components/Next.vue'
 export default {
   name: 'App',
   components: {
     Header,
     VideoBanner,
-    Notifications
+    Notifications,
+    Next
   }
 }
 </script>
